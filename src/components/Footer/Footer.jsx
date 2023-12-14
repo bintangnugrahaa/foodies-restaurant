03c993d9f9ee97f0f8cd66407a98e9fd2ecb7cb2
iNd0n3s1A_cyb3R_Cr1M3 <djakarta1973@gmail.com>
2023-12-15 00:01:37 +0700

    fix footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   FaFacebook,
   FaInstagram,
@@ -12,15 +12,16 @@ const Footer = () => {
   return (
     <div className="bg-gray-100 dark:bg-gray-950">
       <section className="max-w-[1200px] mx-auto">
-        <div className=" grid md:grid-cols-3 py-5">
-          <div className=" py-8 px-4 ">
+        <div className="grid md:grid-cols-3 py-5">
+          <div className="py-8 px-4">
             <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
               <img src={footerLogo} alt="Logo" className="max-w-[50px]" />
               FOODIE
             </h1>
             <p className="">
-              Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Possimus, voluptate.{" "}
+              Nikmati sensasi kuliner terbaik dengan cita rasa yang autentik.
+              Kami berkomitmen untuk memberikan pengalaman makanan yang tak
+              terlupakan.
             </p>
             <br />
             <div className="flex items-center gap-3">
@@ -47,9 +48,9 @@ const Footer = () => {
               </a>
             </div>
           </div>
-          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10 ">
+          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
             <div className="">
-              <div className="py-8 px-4 ">
+              <div className="py-8 px-4">
                 <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
                   Usefull Links
                 </h1>
@@ -57,42 +58,38 @@ const Footer = () => {
                   <li className="cursor-pointer">Home</li>
                   <li className="cursor-pointer">About</li>
                   <li className="cursor-pointer">Testimonial</li>
-                  <li className="cursor-pointer">Login</li>
                 </ul>
               </div>
             </div>
             <div className="">
-              <div className="py-8 px-4 ">
+              <div className="py-8 px-4">
                 <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
-                  Links
+                  Best Seller
                 </h1>
                 <ul className="flex flex-col gap-3">
-                  <li className="cursor-pointer">Home</li>
-                  <li className="cursor-pointer">About</li>
-                  <li className="cursor-pointer">Testimonial</li>
-                  <li className="cursor-pointer">Login</li>
+                  <li className="cursor-pointer">Biryani</li>
+                  <li className="cursor-pointer">Chicken Kari</li>
+                  <li className="cursor-pointer">Cold Coffee</li>
                 </ul>
               </div>
             </div>
             <div className="">
-              <div className="py-8 px-4 ">
+              <div className="py-8 px-4">
                 <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
-                  Links
+                  Cabang Foodie
                 </h1>
-                {/* <ul className="list-disc list-inside"> */}
                 <ul className="flex flex-col gap-3">
-                  <li className="cursor-pointer">Home</li>
-                  <li className="cursor-pointer">About</li>
-                  <li className="cursor-pointer">Testimonial</li>
-                  <li className="cursor-pointer">Login</li>
+                  <li className="cursor-pointer">Jakarta Selatan</li>
+                  <li className="cursor-pointer">Bogor</li>
+                  <li className="cursor-pointer">Depok</li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
         <div>
-          <div className="text-center py-10 border-t-2 border-gray-300/50">
-            @copyright 2024 All rights reserved || Made with ❤️ by Dilshad
+          <div className="text-center py-14 border-t-2 border-gray-300/50">
+            Create with ❤️ by Stareighteen.
           </div>
         </div>
       </section>
